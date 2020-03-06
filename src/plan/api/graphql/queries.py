d4d7e001ff4b331ec87c9be49d64db4d13a92e52
David Torralba Goitia <david.torralba@babylonhealth.com>
2020-03-06 11:47:22 +0000

    :sparkles: GraphQL API. Add GetPlan

--- src/plan/api/graphql/queries.py
+++ src/plan/api/graphql/queries.py
@@ -6,11 +6,15 @@ from src.plan.models import Exercise, Plan, Session
 
 class Query(object):
     plans = graphene.List(types.PlanGraphqlType)
+    plan = graphene.Field(types.PlanGraphqlType, plan_id=graphene.String())
     sessions = graphene.List(types.SessionGraphqlType)
     exercise = graphene.Field(
         types.ExerciseGraphqlType, exercise_id=graphene.String()
     )
     exercises = graphene.List(types.ExerciseGraphqlType)
+
+    def resolve_plan(self, info, plan_id):
+        return Plan.objects.get(pk=plan_id)
 
     def resolve_plans(self, info, **kwargs):
         return Plan.objects.all()
