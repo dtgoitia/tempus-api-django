8f0761efb820ea433de1d96ba2496f8172d21514
David Torralba Goitia <david.torralba@babylonhealth.com>
2020-03-29 21:33:18 +0100

    :sparkles: Add GraphQL factory for testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,15 @@
+import pytest
+from graphene.test import Client
+
+from src.schema import schema
+
+
+@pytest.fixture
+def graphql_client() -> Client:
+    """Create an return a GraphQL client."""
+    return Client(schema)
+
+
 def pytest_configure(config):
     config.addinivalue_line(
         "markers", "focus: mark to target specific tests and skip the rest"
